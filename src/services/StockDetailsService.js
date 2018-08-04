b2d20a1c7162325fa29bfee594c369d0c8347c18
EJ Hammond <ehammond@vistaprint.com>
2018-08-04 18:49:29 -0400

    Hit real api

--- src/services/StockDetailsService.js
+++ src/services/StockDetailsService.js
@@ -1,19 +1,18 @@
 export default class StockDetailsService {
-    static async getStockDetails(ticker) {
-        const data = await new Promise(resolve => {
-            const data = [
-                { date: '2018-07-01', high: 10, low: 5, open: 6, close: 7 },
-                { date: '2018-07-02', high: 9, low: 4, open: 7, close: 5 },
-                { date: '2018-07-03', high: 10, low: 5, open: 5, close: 7 },
-                { date: '2018-07-04', high: 10, low: 6, open: 7, close: 9 },
-                { date: '2018-07-05', high: 12, low: 9, open: 9, close: 12 },
-                { date: '2018-07-06', high: 19, low: 10, open: 12, close: 18 },
-                { date: '2018-07-07', high: 30, low: 17, open: 18, close: 30 },
-            ];
-
-            setTimeout(resolve(data), 2000);
+    static async getStockDetails(symbol) {
+        const response = await fetch(`http://steveberg.herokuapp.com/symbol/${symbol.toLowerCase()}.json`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            }
         });
 
-        return data;
+        if (!response.ok) {
+            throw new Error(`Data for ${symbol} was not found`);
+        }
+
+        const responseJSON = response.json();
+
+        return responseJSON.history;
     }
 }
